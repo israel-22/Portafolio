e98cd80a0d5778c312abb585896b284fd64a496b
Israel <hancellsud@gmail.com>
2025-11-30 22:09:43 -0500

    cambios y pruebas

---
+++ src/components/iconPipBoy/IconPipboy.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./iconPipboy.css";
+
+function IconPipboy({ src, alt }) {
+  return (
+    <img src={src} alt={alt} className="pipboy-icon" />
+  );
+}
+
+export default IconPipboy;
