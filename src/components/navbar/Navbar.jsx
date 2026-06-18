e0770877003319ea2390f80b14f3e84e2c26ddc6
Israel <hancellsud@gmail.com>
2026-06-17 21:53:19 -0500

    Fix GitHub Pages deployment

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -22,9 +22,9 @@ export default function Navbar() {
           <NavLink className="nav-item" to="/contact">[F4] CONTACT</NavLink>
         </li>
 
-        <li>
+        {/*<li>
           <NavLink className="nav-item" to="/skins">[F5] SKINS</NavLink>
-        </li>
+        </li>*/}
 
       </ul>
     </nav>
